7037c8ef59543ee961c14bfe54377f5ae5574d9a
Vladimir Vlasoff <holdencolfild19@gmail.com>
2020-05-08 22:29:39 +0300

    Добавьте файлы проекта.

---
+++ PlaneOOP/Airport.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlaneOOP
+{
+    class Airport
+    {
+        private SortedList<DateTime,Plane> airport;
+        public Airport()
+        {
+            airport = new SortedList<DateTime, Plane>();
+        }
+        public void AddPlane(Plane plane)
+        {
+            if(plane != null)
+            {
+                airport.Add(plane.DepartureTime, plane);
+            }          
+        }
+        public void AddPlanes(params Plane[] planes)
+        {
+            if(planes != null || planes.Length != 0)
+            {
+                foreach(Plane plane in planes)
+                {
+                    airport.Add(plane.DepartureTime, plane);
+                }
+            }
+        }
+        public void GetInfo(int index)
+        {
+            if(index < 0 || index > airport.Count - 1)
+            {
+                throw new IndexOutOfRangeException("Такого рейса не существует");
+            }
+            var planes = airport.Values;
+            Console.WriteLine("Пункт назначения: " + planes[index].DestinationName);
+            Console.WriteLine("Номер рейса: " + planes[index].FlightNumber);
+            Console.WriteLine("Пункт назначения: " + planes[index].DepartureTime);
+        }
+        public void GetPlanesInOneHour(DateTime time)
+        {
+            DateTime PlusHour = new DateTime(time.Year, time.Month, time.Day,
+                time.Hour + 1, time.Minute, time.Second, DateTimeKind.Local);
+            Console.WriteLine("В течение часа вылетят следующие самолеты: ");
+            foreach(DateTime PlaneTime in airport.Keys)
+            {
+                if(PlaneTime > time && PlaneTime < PlusHour)
+                {                   
+                    GetInfo(airport.IndexOfKey(PlaneTime));
+                }
+            }
+        }
+        public void GetPlanesByDestinationName(string DestinationName)
+        {
+            Console.WriteLine($"В пункт назначения {DestinationName} летят следующие самолеты: ");
+            foreach(Plane plane in airport.Values)
+            {
+                if (plane.DestinationName.Equals(DestinationName))
+                {
+                    GetInfo(airport.IndexOfValue(plane));
+                }
+            }
+        }      
+    }
+}
---
+++ PlaneOOP/Plane.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaneOOP
+{
+    class Plane
+    {
+        private string destinationName;
+        private uint flightNumber;
+        private DateTime departureTime;
+
+        public string DestinationName
+        {
+            get => destinationName;
+            set => destinationName = value;
+        }
+        public uint FlightNumber
+        {
+            get => flightNumber;
+            set => flightNumber = value;
+        }
+        public DateTime DepartureTime
+        {
+            get => departureTime;
+            set => departureTime = value;
+        }
+
+        public Plane(string destinationName, uint flightNumber, DateTime departureTime)
+        {
+            this.destinationName = destinationName;
+            if(flightNumber < 100000 || flightNumber > 999999)
+            {
+                throw new ArgumentException("Номер рейса должен быть шестизначным");
+            }
+            this.flightNumber = flightNumber;
+            this.departureTime = departureTime;
+        }
+
+        public static bool operator >(Plane one, Plane two)
+        {
+            return (one.DepartureTime < two.DepartureTime) ? true : false;
+        }
+
+        public static bool operator <(Plane one, Plane two)
+        {
+            return (one.DepartureTime < two.DepartureTime) ? true : false;
+        }
+
+    }
+}
